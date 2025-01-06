047567e66e2ce07d474e4847834fd186ede32bad
NearlyHeadlessJack <wang@rjack.cn>
2025-01-06 15:00:41 +0800

    1.3.3-rc1

--- weauth/constants/core_constant.py
+++ weauth/constants/core_constant.py
@@ -13,9 +13,9 @@ PACKAGE_NAME = 'weauth'
 CLI_COMMAND = PACKAGE_NAME
 
 # WeAuth Version Storage
-VERSION_PYPI:str  = '1.3.1'   #
-VERSION: str = '1.3.1'  # 版本号
-BUILD_VERSION: str = '1.3.0.2025.1.6.5'   # 内部版本号
+VERSION_PYPI:str  = '1.3.3-rc1'   #
+VERSION: str = '1.3.3rc1'  # 版本号
+
 
 # URLs
 GITHUB_URL = r'https://github.com/TomatoCraftMC/WeAuth'
