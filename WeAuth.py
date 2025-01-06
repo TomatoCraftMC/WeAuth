a6d5afe85948265aa73adae18fa90af87ae6c6e1
NearlyHeadlessJack <wang@rjack.cn>
2025-01-06 15:39:31 +0800

    程序入口

---
+++ WeAuth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.10
+# -*- coding: utf-8 -*-
+# author： NearlyHeadlessJack
+# email: wang@rjack.cn
+# datetime： 2025/1/6 15:39 
+# ide： PyCharm
+# file: WeAuth.py
+from weauth.__main__ import main
+
+if __name__ == '__main__':
+	main()
