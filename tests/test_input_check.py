5c384a4b55ab6895406a8821057b7a93db8f4f4d
NearlyHeadlessJack <wang@rjack.cn>
2025-01-07 16:43:21 +0800

    fix: op_add input check

--- tests/test_input_check.py
+++ tests/test_input_check.py
@@ -7,16 +7,16 @@ class MyTestCase(unittest.TestCase):
         test2 = '92nfkdnk1'
         test3 = '@j1kmf'
         test4 = 'ADDDDD___882'
-        test5 = 'B B'
         test6 = '____'
         test7 = '-'
+        test8 = '12ab@@p'
         self.assertEqual(check_op_id_input(test1), False)  # add assertion here
         self.assertEqual(check_op_id_input(test2), False)
         self.assertEqual(check_op_id_input(test3), True)
         self.assertEqual(check_op_id_input(test4), False)
-        # self.assertEqual(check_op_id_input(test5), True)
         self.assertEqual(check_op_id_input(test6), False)
         self.assertEqual(check_op_id_input(test7), True)
+        self.assertEqual(check_op_id_input(test8), True)
 
 
 if __name__ == '__main__':
