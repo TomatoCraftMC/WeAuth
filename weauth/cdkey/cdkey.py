873c897fa7f322b95e06ea7e1ef4190884a6ab5f
NearlyHeadlessJack <wang@rjack.cn>
2025-01-09 23:03:39 +0800

    feat(cdkey): 生成礼物逻辑优化
    相同arg、num与注释的礼物会认为是同一个礼物，直接更改其total

--- weauth/cdkey/cdkey.py
+++ weauth/cdkey/cdkey.py
@@ -18,21 +18,24 @@ class CDKey:
     @staticmethod
     def create_gift(gift_arg: str, gift_num: int, gift_total: int, gift_comment='无礼物注释') -> int:
         sha1_hash = hashlib.sha1()
-        gift_str = gift_arg + gift_comment + str(gift_total) + str(gift_num)
+        gift_str = gift_arg + gift_comment + str(gift_num)
         sha1_hash.update(gift_str.encode('utf-8'))
         gift_hash = sha1_hash.hexdigest()[-7:]  # 用来区别礼物的唯一标识
 
         context = {gift_hash: {'gift_arg': gift_arg,  # 礼物 如 minecraft:command_block  可以带NBT标签
-                               'gift_num': gift_num,  # 礼物个数   单次给予的礼物的数量
+                               'gift_num': gift_num,  # 礼物数   单次给予的礼物的数量
                                'gift_total': gift_total,  # 赠礼次数   这个礼物一共可以兑换多少次
                                'valid': True,  # 是否有效   默认为True  若停用礼物,改成False
                                'gift_comment': gift_comment  # 注释
                                }}
-
         try:
             with open('gift_list.yaml', 'r', encoding='utf-8') as f:
                 result = yaml.load(f.read(), Loader=yaml.FullLoader)
-                result.update(context)
+                try:
+                    result[gift_hash]['gift_total'] += gift_total
+                    print('-已存在相同礼物,将增加其总数量')
+                except KeyError:
+                    result.update(context)
             with open('gift_list.yaml', 'w+') as f:
                 yaml.dump(data=result, stream=f, allow_unicode=True, sort_keys=False)
         except FileNotFoundError:
@@ -58,4 +61,4 @@ class CDKey:
 
 if __name__ == '__main__':
     text = r'minecraft:grass_blck'
-    CDKey.create_gift(gift_arg=text, gift_num=64, gift_total=15, gift_comment='草方块')
+    CDKey.create_gift(gift_arg=text, gift_num=6, gift_total=1, gift_comment='草方块')
