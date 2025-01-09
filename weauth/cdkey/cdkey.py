04409122e8ef6f140d4e2538d68fccb7f93b812d
NearlyHeadlessJack <wang@rjack.cn>
2025-01-09 22:37:37 +0800

    feat(cdkey): gift生成逻辑,利用hash和字典辅助查询和排重
    数量、个数和arg、注释完全相同的礼物会被排除

--- weauth/cdkey/cdkey.py
+++ weauth/cdkey/cdkey.py
@@ -9,23 +9,35 @@ from weauth.constants.core_constant import CDKEY_LENGTH_ONE_PIECE
 import string
 import secrets
 import yaml
-
+import hashlib
 
 class CDKey:
     def __init__(self, cdkey: str):
         self.cdkey = cdkey
 
     @staticmethod
-    def create_gift(gift_name: str, gift_num_total: int, gift_comment: str) -> int:
-        context = {gift_comment: {'gift_name': gift_name, 'gift_num_total': gift_num_total, 'valid': True}}
+    def create_gift(gift_arg: str, gift_num: int, gift_total: int, gift_comment='无礼物注释') -> int:
+        sha1_hash = hashlib.sha1()
+        gift_str = gift_arg + gift_comment + str(gift_total) + str(gift_num)
+        sha1_hash.update(gift_str.encode('utf-8'))
+        gift_hash = sha1_hash.hexdigest()[-7:]  # 用来区别礼物的唯一标识
+
+        context = {gift_hash: {'gift_arg': gift_arg,  # 礼物 如 minecraft:command_block  可以带NBT标签
+                               'gift_num': gift_num,  # 礼物个数   单次给予的礼物的数量
+                               'gift_total': gift_total,  # 赠礼次数   这个礼物一共可以兑换多少次
+                               'valid': True,  # 是否有效   默认为True  若停用礼物,改成False
+                               'gift_comment': gift_comment  # 注释
+                               }}
 
         try:
             with open('gift_list.yaml', 'r', encoding='utf-8') as f:
                 result = yaml.load(f.read(), Loader=yaml.FullLoader)
-                print(repr(result))
+                result.update(context)
+            with open('gift_list.yaml', 'w+') as f:
+                yaml.dump(data=result, stream=f, allow_unicode=True, sort_keys=False)
         except FileNotFoundError:
             with open('gift_list.yaml', 'w+') as f:
-                yaml.dump(data=context, stream=f, allow_unicode=True)
+                yaml.dump(data=context, stream=f, allow_unicode=True, sort_keys=False)
 
         return 0
         pass
@@ -44,4 +56,6 @@ class CDKey:
             raise ValueError("CDKEY_LENGTH_ONE_PIECE must be defined and should be a positive integer")
 
 
-CDKey.create_gift('测试', 1, '测试')
+if __name__ == '__main__':
+    text = r'minecraft:grass_blck'
+    CDKey.create_gift(gift_arg=text, gift_num=64, gift_total=15, gift_comment='草方块')
