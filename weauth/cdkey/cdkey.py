e8a51de0de56b9fc66bcb7dee6a1f963f57f29d2
NearlyHeadlessJack <wang@rjack.cn>
2025-01-10 17:31:48 +0800

    feat(cdkey): 增加用户生成cdkey总入口

--- weauth/cdkey/cdkey.py
+++ weauth/cdkey/cdkey.py
@@ -5,6 +5,8 @@
 # datetime： 2025/1/9 20:36 
 # ide： PyCharm
 # file: cdkey.py
+import sys
+
 from weauth.constants.core_constant import CDKEY_LENGTH_ONE_PIECE
 import string
 import secrets
@@ -16,7 +18,28 @@ class CDKey:
         self.cdkey = cdkey
 
     @staticmethod
-    def create_gift(gift_arg: str, gift_num: int, gift_total: int, gift_comment='无礼物注释') -> int:
+    def create_gift_interface() -> None:
+        gift_comment: str = input('-请输入礼物注释,并按回车确认。例如: 火把/钻石/给小张的礼物\n')
+        gift_num: int = int(input('-请输入单次兑换所给予的数量,并按回车确认。例如: 6\n'))
+        gift_arg: str = input('-请输入礼物,可以带有NBT标签。例如：\n'
+                              r'minecraft:torch 或minecraft:netherite_pickaxe{CanDestroy:[&#34;minecraft:stone&#34;]}'
+                              '\n')
+        gift_total: int = int(input('-请输入生成CDKey数量\n'))
+        try:
+            gift_hash = CDKey.create_gift(gift_arg=gift_arg,
+                                          gift_num=gift_num,
+                                          gift_total=gift_total,
+                                          gift_comment=gift_comment)
+            CDKey.generate_cdkey(gift_hash=gift_hash)
+            print('-CDKey生成成功!')
+            sys.exit(0)
+        except Exception:
+            print('-生成失败')
+            sys.exit(1)
+
+
+    @staticmethod
+    def create_gift(gift_arg: str, gift_num: int, gift_total: int, gift_comment='无礼物注释') -> str:
         sha1_hash = hashlib.sha1()
         gift_str = gift_arg + gift_comment + str(gift_num)
         sha1_hash.update(gift_str.encode('utf-8'))
@@ -33,24 +56,26 @@ class CDKey:
                 result = yaml.load(f.read(), Loader=yaml.FullLoader)
                 try:
                     result[gift_hash]['gift_total'] += gift_total
-                    print('-已存在相同礼物,将增加其总数量')
+                    print(f'-已存在相同礼物 {gift_hash},将增加其总数量')
                 except KeyError:
                     result.update(context)
+                    print(f'-已生成新的礼物 {gift_hash}')
             with open('gift_list.yaml', 'w+') as f:
                 yaml.dump(data=result, stream=f, allow_unicode=True, sort_keys=False)
+            return gift_hash
         except FileNotFoundError:
             with open('gift_list.yaml', 'w+') as f:
                 yaml.dump(data=context, stream=f, allow_unicode=True, sort_keys=False)
+            print(f'-已生成新的礼物 {gift_hash},保存在./gift_list.yaml中')
+            return gift_hash
 
-        return 0
-        pass
+
 
     @staticmethod
     def generate_cdkey_one() -> str:
         try:
             if CDKEY_LENGTH_ONE_PIECE <= 0:
                 return ''
-
             parts = [
                 ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(CDKEY_LENGTH_ONE_PIECE))
                 for _ in range(4)]
@@ -58,6 +83,10 @@ class CDKey:
         except NameError:
             raise ValueError("CDKEY_LENGTH_ONE_PIECE must be defined and should be a positive integer")
 
+    @staticmethod
+    def generate_cdkey(gift_hash: str) -> None:
+
+        pass
 
 if __name__ == '__main__':
     text = r'minecraft:grass_blck'
