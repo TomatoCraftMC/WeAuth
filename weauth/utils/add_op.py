371c6988d2319921ddc5a7f7b8e369e9fc4e74bc
NearlyHeadlessJack <wang@rjack.cn>
2025-01-11 04:46:31 +0800

    fix(utils.add_op): 恶性bug修复，文件IO

--- weauth/utils/add_op.py
+++ weauth/utils/add_op.py
@@ -15,10 +15,10 @@ def add_op(op_id:str):
         print('-输入ID不合法!')
         sys.exit(0)
     try:
-        with open('ops.yaml','r') as f:
+        with open('ops.yaml', 'r', encoding='utf-8') as f:
             result = yaml.load(f.read(), Loader=yaml.FullLoader)
-            op_list = result['ops']
-            op_list.append(op_id)
+        op_list = result['ops']
+        op_list.append(op_id)
         with open('ops.yaml','w') as f:
             context = {
                 'ops':op_list
@@ -27,7 +27,7 @@ def add_op(op_id:str):
     except FileNotFoundError:
         with open('ops.yaml', 'w+') as f:
             context = {'ops': [op_id]}
-        yaml.dump(data=context, stream=f, allow_unicode=True)
+            yaml.dump(data=context, stream=f, allow_unicode=True)
 def check_op_id_input(op_id:str)->bool:
     pattern = re.compile(r'\W')
     if re.search(pattern, op_id) is None:
