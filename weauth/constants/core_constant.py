f0bf9a3538babd70826e04734e6baf40d12f24a5
NearlyHeadlessJack <wang@rjack.cn>
2025-01-11 08:17:40 +0800

    test ci

--- weauth/constants/core_constant.py
+++ weauth/constants/core_constant.py
@@ -16,8 +16,8 @@ PACKAGE_NAME = 'weauth'
 CLI_COMMAND = PACKAGE_NAME
 
 # WeAuth Version Storage
-VERSION_PYPI: str = '1.5.1'
-VERSION: str = '1.5.1'
+VERSION_PYPI: str = '1.5.2'
+VERSION: str = '1.5.2'
 
 
 # URLs
