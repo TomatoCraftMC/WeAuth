c8ce2b25c904251d31fdc3c4c8533e114d9006e2
差点没头的杰克 <wang@rjack.cn>
2025-01-13 13:43:01 +0800

    feature/new_web_server (#37)
    * feat(boostrap): 使用gevent作为web服务器

--- weauth/constants/core_constant.py
+++ weauth/constants/core_constant.py
@@ -17,8 +17,8 @@ PACKAGE_NAME = 'weauth'
 CLI_COMMAND = PACKAGE_NAME
 
 # WeAuth Version Storage
-VERSION_PYPI: str = '1.5.4'
-VERSION: str = '1.5.4'
+VERSION_PYPI: str = '1.5.5'
+VERSION: str = '1.5.5'
 
 
 # URLs
