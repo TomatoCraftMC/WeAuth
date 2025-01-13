f3989ed5eadc495d5affd681118909ea9d72dcb4
NearlyHeadlessJack <wang@rjack.cn>
2025-01-13 12:34:46 +0800

    v1.5.4

--- weauth/constants/core_constant.py
+++ weauth/constants/core_constant.py
@@ -17,8 +17,8 @@ PACKAGE_NAME = 'weauth'
 CLI_COMMAND = PACKAGE_NAME
 
 # WeAuth Version Storage
-VERSION_PYPI: str = '1.5.4rc1'
-VERSION: str = '1.5.4-rc1'
+VERSION_PYPI: str = '1.5.4'
+VERSION: str = '1.5.4'
 
 
 # URLs
