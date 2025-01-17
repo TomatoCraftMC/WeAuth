f9cec7c97af0d3b0bb0ba791c78453bf6329e648
差点没头的杰克 <wang@rjack.cn>
2025-01-18 02:49:45 +0800

    docs 说明文档 (#45)

---
+++ docs/source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'WeAuth'
+copyright = '2025, NearlyHeadlessJack'
+author = 'NearlyHeadlessJack'
+from weauth.constants.core_constant import VERSION
+
+release = 'v' + VERSION
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
+html_logo = '../../logo/long_banner.png'
---
+++ docs/source/index.rst
@@ -0,0 +1,25 @@
+欢迎来到WeAuth使用手册！
+===================
+欢迎您使用WeAuth，这是一个轻量化的web服务，用来连接微信公众号与您的Minecraft服务器。
+
+特别感谢 `Fallen_Breath <https://github.com/Fallen-Breath>`__ 开发的 `MCDReforged <https://github.com/MCDReforged/MCDReforged>`__ 带给我们启发，并展示出Python社区无穷的活力。
+
+WeAuth在 `GNU GENERAL PUBLIC LICENSE Version 3 (GPLv3) <https://github.com/TomatoCraftMC/WeAuth/blob/main/LICENSE>`__ 协议下开源，因此WeAuth是一款自由软件，您可以自由下载并部署到您的MC服务系统当中，无论是收费服务器或者是公益服。
+
+如果您开发的应用需要WeAuth作为依赖，或者您修改并重新发布了WeAuth，请遵守GPLv3协议开源。
+
+如果您使用WeAuth仅用于微信公众平台验证第三方服务器使用，请直接阅读 :doc:`/quick_start/only_for_wechat_confirm` 。
+
+.. toctree::
+   :maxdepth: 2
+   :caption: 首页
+
+   下载和安装<quick_start/index.rst>
+   微信公众号后台设置<wechat_config/index.rst>
+   连接游戏服务器<mc_config/index.rst>
+   启动参数与命令<command/index.rst>
+   参与WeAuth开发<dev/index.rst>
+   开源组件许可<lis.rst>
+
+
+
